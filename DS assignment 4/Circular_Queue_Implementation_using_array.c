b7172b82f7425cd4cc088bcc2c518240232d149c
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2020-10-06 22:51:42 +0530

    Circular Queue implementation using array

---
+++ DS assignment 4/Circular_Queue_Implementation_using_array.c
@@ -0,0 +1,106 @@
+//Data Structures
+
+//Write a program to implement Circular Queue using array
+
+//Name-Sai Prashant Saxena
+//Roll no-022 20902719
+
+#include <stdio.h>
+#define Max 100
+void ask();
+int main()
+{
+    int queue[Max];
+    int front = 0, rear = -1;
+    printf("Hello there!!\n");
+
+    int userValue;
+askAgain:
+    ask();
+    scanf("%d", &userValue);
+    if (userValue == 1)
+    {
+        int value;
+        printf("\nEnter value to be added\n");
+        scanf("%d", &value);
+        if (rear == Max - 1)
+        {
+            printf("\nQueue Overflow");
+        }
+        else
+        {
+            rear++;
+            queue[rear] = value;
+        }
+        printf("\nQueue after this operation is..");
+        printValue(queue, front, rear);
+    }
+    else if (userValue == 2)
+    {
+        if (front > rear)
+        {
+            printf("\nQueue is already empty!!");
+        }
+        else if (front == Max - 1)
+        {
+            front = 0;
+            rear = -1;
+        }
+        else
+        {
+            front++;
+            printf("\nQueue after this operation is..");
+            printValue(queue, front, rear);
+        }
+    }
+    else if (userValue == 3)
+    {
+        if (front == -1 || front > rear)
+        {
+            printf("\nQueue is already empty!!");
+        }
+        else
+        {
+
+            printf("\nThe front value of queue is %d", queue[front]);
+        }
+    }
+    else if (userValue == 4)
+    {
+        if (rear == -1)
+        {
+            printf("Queue is already empty!!!");
+        }
+        printf("\nThe rear value of queue is %d", queue[rear]);
+    }
+    else
+    {
+        goto end;
+    }
+    goto askAgain;
+
+end:
+    return 0;
+}
+
+void ask()
+{
+    printf("\nPress 1 for enqueue     Press 2 for dequeue   Press 3 for front  Press 4 for rear   Press 0 to exit...\n  ");
+}
+
+void printValue(int queue[], int front, int rear)
+{
+    if (front == -1 || front > rear)
+    {
+        printf(" empty");
+    }
+    else
+    {
+        printf("\n");
+        for (int i = front; i <= rear; i++)
+        {
+            printf("%d ", queue[i]);
+        }
+        printf("\n");
+    }
+}
