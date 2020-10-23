26bda0535edc93082d878811696252840a8728e9
Vishal Paul <72184979+Vishal-27@users.noreply.github.com>
2020-10-23 15:03:15 +0530

    Update Program_to_Create_a_linked_list.c

--- DS assignment 3/Program_to_Create_a_linked_list.c
+++ DS assignment 3/Program_to_Create_a_linked_list.c
@@ -1,91 +1,60 @@
-//DSA
+// Program to create a linked list.
 
-//Write a program to create a linkedList
-
-//Name-Sai Prashant Saxena
-//Roll no-02220902719
-
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 struct node
 {
-    int info;
-    struct node *link;
+	int info;
+	struct node *link;
 };
-void printList(struct node *);
+
+void insertion(struct node *);
+void print(struct node *);
 
 int main()
 {
-    struct node *head = (struct node *)malloc(sizeof(struct node));
-    struct node *temp = head;
-    printf("Hello there!!");
-    printf("\nWant to create a linked list press Y.....\n\t\t");
-    char c;
-    scanf("%c", &c);
-    if (c == 'Y' || c == 'y')
-    {
-        printf("Enter the value of head\n  ");
-        int value;
-        scanf("%d", &value);
-        head->info = value;
-        head->link = NULL;
-        temp = head;
-    }
-    else
-    {
-        printf("Sorry!!!...Invalid Input\n");
-        printf("Program exited");
-        goto end;
-    }
-
-    int ch;
-    printf("Want to add more values to linked list press 1...\n  ");
-    scanf("%d", &ch);
-    if (ch == 1)
-    {
-    addNode:
-        printf("Enter the next value\n  ");
-        int value;
-        scanf("%d", &value);
-        struct node *new = (struct node *)malloc(sizeof(struct node));
-        new->info = value;
-        new->link = NULL;
-        temp->link = new;
-        temp = temp->link;
-        printf("\nThe new List is:");
-        printList(head);
-    }
-    else
-    {
-        printf("At last the linked list is..");
-        printList(head);
-        goto end;
-    }
-    printf("Want to add more values to linked list press 1...\n  ");
-    scanf("%d", &ch);
-    if (ch == 1)
-    {
-        goto addNode;
-    }
-    else
-    {
-        printf("Your Linked List..\n");
-        printList(head);
-    }
-
-end:
-    return 0;
+	int value,n;
+	struct node *temp=NULL;
+	struct node *start=(struct node *)malloc(sizeof(struct node));
+	printf("\nEnter the value of first node: ");
+	scanf("%d",&value);
+	start->info=value;
+	start->link=NULL;
+	temp=start;
+	
+	printf("\nIf you want to add new node press 1 else press 0 : ");
+	scanf("%d",&n);
+	while(n==1)
+	{
+		insertion(temp);
+		temp=temp->link;
+		printf("\nIf you want to add new node press 1 else press 0 : ");
+		scanf("%d",&n);
+	}
+	printf("\nThe linked list is : ");
+	print(start);
+	return 0;
 }
-void printList(struct node *head)
+
+void insertion(struct node *temp)
 {
-    struct node *temp = head;
-    printf("\n");
-    while (temp != NULL)
-    {
-       	printf("-->%d Address of node: %d",temp->info,temp);
-  	    printf("\t Next:%d\n",temp->link); 
-  	    temp = temp->link;
-    }
-    printf("\n");
+	int value;
+	struct node *next=(struct node *)malloc(sizeof(struct node));
+	printf("\nEnter the value : ");
+	scanf("%d",&value);
+	next->info=value;
+	next->link=NULL;
+	temp->link=next;
+}
+
+void print(struct node *start)
+{
+	struct node *ptr;
+	ptr=start;
+	while(ptr!=NULL)
+	{
+		printf("%d ",ptr->info);
+		ptr=ptr->link;
+	}
 }
